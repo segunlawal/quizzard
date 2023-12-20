fb533ad04a57529c69035275de2126e5d5278f48
Segun <lawalsegun72@gmail.com>
2023-12-20 13:42:31 +0100

    fix: display error message

--- src/app/(app)/dashboard/quiz/begin-quiz/page.tsx
+++ src/app/(app)/dashboard/quiz/begin-quiz/page.tsx
@@ -3,6 +3,7 @@ import QuizQuestions from './QuizQuestions';
 import { redirect } from 'next/navigation';
 import { authOptions } from '@/lib/authOptions';
 import { getServerSession } from 'next-auth';
+import Image from 'next/image';
 
 type Props = {
   params: {};
@@ -12,22 +13,40 @@ type Props = {
 const page = async (props: Props) => {
   const session = (await getServerSession(authOptions)) as Session;
 
-  const data = await getData(props);
-  const searchParams = props.searchParams;
-  const category = searchParams.category;
+  try {
+    const data = await getData(props);
+    const searchParams = props.searchParams;
+    const category = searchParams.category;
 
-  const shuffledData = addShuffledChoices(data.results);
+    const shuffledData = addShuffledChoices(data?.results);
 
-  return (
-    <div>
-      <QuizQuestions
-        data={shuffledData}
-        category={category}
-        takerId={session?.user?.id}
-        takerName={session?.user?.name}
-      />
-    </div>
-  );
+    return (
+      <div>
+        <QuizQuestions
+          data={shuffledData}
+          category={category}
+          takerId={session?.user?.id}
+          takerName={session?.user?.name}
+        />
+      </div>
+    );
+  } catch (error) {
+    // Handle API error here
+    return (
+      <div className="text-center mt-20">
+        <Image
+          src="/assets/error-img.svg"
+          alt="logo"
+          height={0}
+          width={0}
+          style={{ width: '200px', height: 'auto' }}
+          priority
+          className="mx-auto"
+        />
+        Error fetching quiz data. Please try again later.
+      </div>
+    );
+  }
 };
 
 export default page;
@@ -47,6 +66,7 @@ async function getData(props: Props) {
   );
 
   if (!res.ok) {
+    console.error(res);
     throw new Error('Failed to fetch data');
   }
 
